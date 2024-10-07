d1d1ed054361d54fd43a1d862545cf08e1291170
busipatianil <155179624+busipatianil@users.noreply.github.com>
2024-10-07 12:17:37 +0530

    terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,26 +1,12 @@
 terraform {
-  # cloud {
-  #   workspaces {
-  #     name = "learn-terraform-eks"
-  #   }
-  # }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.47.0"
+      version = "~> 5.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6.1"
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.32.0"
     }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0.5"
-    }
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = "~> 2.3.4"
-    }
-  }
-  required_version = "~> 1.3"
+}
 }
